b3de6d91937a1fa3651a410d704342268c9fd239
chuanz <chuanz@genecloud.com>
2017-09-06 12:01:00 -0700

    config flask for aws

--- trip_planner/views.py
+++ trip_planner/views.py
@@ -5,37 +5,46 @@ from flask import jsonify, render_template, request, make_response
 # from flask_login import login_required, current_user
 
 from sqlalchemy import and_, or_
-from trip_planner import app
+from trip_planner import app, AWS_DEPLOY
 from trip_planner.models import *
 
 if not os.environ.get('GOOGLE_API_KEY'):
     sys.stderr.write("No GOOGLE_API_KEY environ\n\n")
     sys.exit(2)
 
-GOOGLE_API_URL = 'https://maps.googleapis.com/maps/api/js?key=%s&libraries=places' % os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_URL = 'https://maps.googleapis.com/maps/api/js?key=%s&libraries=places' % os.environ.get(
+    'GOOGLE_API_KEY')
 """
 Initializes the database.
 The app.cli.command() decorator registers a new command with the flask script.
 """
+
 
 @app.cli.command('createtables')
 def initdb_command():
     db.create_all()
     print 'Initialized the database.'
 
+
 @app.cli.command('droptables')
 def dropdb_command():
     db.drop_all()
     print 'Droped the database.'
 
+
 @app.route('/login')
 def login_page():
     return render_template('login.html')
 
+
 @app.route('/')
 # @login_required
 def index():
-    return render_template('index.html')
+    if AWS_DEPLOY:
+        return render_template('index.html')
+    else:
+        return render_template('index.dev.html')
+
 
 @app.route('/google_map_api.js')
 def get_google_api():
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,91 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const outputPath = "dist";
 
 module.exports = {
   // absolute path, for resolving entry points and loaders from configuration.
   context: __dirname,
 
   entry: {
-    dashboard : [
-      'react-hot-loader/patch',
-      'webpack/hot/only-dev-server',
-      './client/entry.js'
+    dashboard: [
+      "react-hot-loader/patch",
+      "webpack/hot/only-dev-server",
+      "./client/entry.js"
     ],
 
-    common : [
-      'react-hot-loader/patch',
-      'webpack/hot/only-dev-server',
-      'react', 'react-dom','jquery'
-    ],
+    common: [
+      "react-hot-loader/patch",
+      "webpack/hot/only-dev-server",
+      "react",
+      "react-dom",
+      "jquery"
+    ]
   },
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, outputPath),
     filename: "[name].bundle.js",
-    publicPath: '/'
+    publicPath: "/"
   },
 
   devServer: {
     // enable HMR on the server
-    hot: true, 
+    hot: true,
     // match the output path
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, outputPath),
     // match the output `publicPath`
-    publicPath: '/',
+    publicPath: "/",
     historyApiFallback: true
-
   },
 
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/], 
+        test: [/\.jsx?$/, /\.js?$/],
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: { presets: ["es2015","stage-2","react"] }
+        loader: "babel-loader",
+        query: { presets: ["es2015", "stage-2", "react"] }
       },
       // json loader
       { test: /\.json$/, loader: "json-loader" },
       // sass loader
       {
         test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      },
-      { // regular css files
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
-          // https://github.com/webpack-contrib/css-loader/issues/228
-        }),
-      },
-      { 
-        test: /\.(otf|png|woff|woff2?|ttf|eot)$/, 
-        loader: 'url-loader'
+        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
       },
       {
-        test: /\.svg$/, 
-        loader: 'react-svg-loader?es5=1'
+        // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader?importLoaders=1"
+          // https://github.com/webpack-contrib/css-loader/issues/228
+        })
+      },
+      {
+        test: /\.(otf|png|woff|woff2?|ttf|eot)$/,
+        loader: "url-loader"
+      },
+      {
+        test: /\.svg$/,
+        loader: "react-svg-loader?es5=1"
       }
     ]
   },
 
-  plugins:[
+  plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['common','manifest'], // Specify the common chunk's name.
-      filename : '[name].js' // the file name of the common chunk
+      name: ["common", "manifest"], // Specify the common chunk's name.
+      filename: "[name].js" // the file name of the common chunk
     }),
-    new ExtractTextPlugin('style/style.css')
+    new ExtractTextPlugin("style/style.css")
   ],
 
-  devtool: 'source-maps',
+  devtool: "source-maps",
 
   resolve: {
-    extensions: [".js", ".jsx" ]
+    extensions: [".js", ".jsx"]
   }
-}
+};
